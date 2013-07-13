d3f8746a13431f093ed6278585028e853f4888c3
Varun S Lingaraju <warunsl@gmail.com>
2013-07-13 00:15:33 -0700

    version 0.1

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
-import sys
-import flask
 import subprocess
 
+import oauth2 as oauth
+import requests
+from requests_oauthlib import OAuth1
+
 from flask import Flask
-from linkedin import linkedin
-from flask import render_template, request, redirect, url_for
+from flask import render_template
+
 
 host = subprocess.check_output('hostname').strip()
 try:
@@ -18,22 +20,36 @@ except OSError:
 app = Flask(__name__)
 
 
-def initialize():
-    CONSUMER_KEY = ''
-    CONSUMER_SECRET = ''
-    USER_TOKEN = ''
-    USER_SECRET = ''
-    RETURN_URL = ''
+CONSUMER_KEY = 'sjjy1qiurkig'
+CONSUMER_SECRET = 'hiS7IQizThEHqkIZ'
+OAUTH_TOKEN = '7790c0fa-8aed-4a9e-9373-f29d36225e0d'
+OAUTH_TOKEN_SECRET = '723f556c-5803-44c2-a62e-71015eeda040'
 
-    authentication = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET,
-                                                              USER_TOKEN, USER_SECRET,
-                                                              RETURN_URL, linkedin.PERMISSIONS.enums.values())
-    application = linkedin.LinkedInApplication(authentication)
-    application.get_profile()
+CONNECTIONS_URL = 'http://api.linkedin.com/v1/people/~/connections'
+AUTHORIZATION_URL = 'https://www.linkedin.com/uas/oauth2/authorization'
+ACCESS_TOKEN_URL = 'https://www.linkedin.com/uas/oauth2/accessToken'
+
+url = "http://api.linkedin.com/v1/people/~"
+
+
+consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+token = oauth.Token(key=OAUTH_TOKEN, secret=OAUTH_TOKEN_SECRET)
+
+# print consumer # oauth_consumer_key=sjjy1qiurkig&oauth_consumer_secret=hiS7IQizThEHqkIZ
+# print token # oauth_token_secret=723f556c-5803-44c2-a62e-71015eeda040&oauth_token=7790c0fa-8aed-4a9e-9373-f29d36225e0d
+
+oauth1 = OAuth1(CONSUMER_KEY, client_secret=CONSUMER_SECRET, resource_owner_key=OAUTH_TOKEN, resource_owner_secret=OAUTH_TOKEN_SECRET)
+client = oauth.Client(consumer, token)
+resp, content = client.request(url)
+# print resp
+# print content
+# print r.content
 
 
 @app.route("/")
-def start():
+def index():
+    r = requests.get(url=CONNECTIONS_URL, auth=oauth1)
+    print r.content
     return render_template('index.html')
 
 
--- scripts/auth.py
+++ scripts/auth.py
@@ -17,8 +17,8 @@ url = "http://api.linkedin.com/v1/people/~"
 consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
 token = oauth.Token(key=OAUTH_TOKEN, secret=OAUTH_TOKEN_SECRET)
 
-print consumer # oauth_consumer_key=sjjy1qiurkig&oauth_consumer_secret=hiS7IQizThEHqkIZ
-print token # oauth_token_secret=723f556c-5803-44c2-a62e-71015eeda040&oauth_token=7790c0fa-8aed-4a9e-9373-f29d36225e0d
+# print consumer # oauth_consumer_key=sjjy1qiurkig&oauth_consumer_secret=hiS7IQizThEHqkIZ
+# print token # oauth_token_secret=723f556c-5803-44c2-a62e-71015eeda040&oauth_token=7790c0fa-8aed-4a9e-9373-f29d36225e0d
 
 oauth1 = OAuth1(CONSUMER_KEY, client_secret=CONSUMER_SECRET, resource_owner_key=OAUTH_TOKEN, resource_owner_secret=OAUTH_TOKEN_SECRET)
 client = oauth.Client(consumer, token)
